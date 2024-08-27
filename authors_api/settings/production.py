28a175acaf59023922b24c040f9070401accfad8
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2024-08-27 21:49:48 +0530

    setting up django settings

---
+++ authors_api/settings/production.py
@@ -0,0 +1,7 @@
+from .base import * #noqa
+from .base import env
+
+ADMINS = [("DEEPJYOTI BHATTACHARJEE","deepjyotibhattacharjee217@gmail.com")]
+
+# TODO : add domain name of production server
+CSRF_TRUSTED_ORIGINS = [""]
